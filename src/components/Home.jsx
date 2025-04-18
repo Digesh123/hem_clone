f62819b6787a85795364a0cfbc942463638b262f
Digesh123 <digeshghevade123@gmail.com>
2025-04-19 02:23:47 +0530

    P10

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import HomeSlider from './HomeSlider'
 import WhyChooseUs from './WhyChooseUs'
 import Downloadpdf from './Downloadpdf'
-// import { root1 } from 'postcss'
+import { root1 } from 'postcss'
 
-// const root1 = ReactDOM.createRoot(document.getElementById('root1'));
-//  root1.render(
-//         <Downloadpdf
-//         />
-// );
+const root1 = ReactDOM.createRoot(document.getElementById('root1'));
+ root1.render(
+        <Downloadpdf
+        />
+);
 
 function Home({setActivePage}) {
     return (
