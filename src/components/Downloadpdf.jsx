03dd2f63c1717314f54a42aeeea3e9fcd3fb5d6d
Digesh123 <digeshghevade123@gmail.com>
2025-04-19 02:11:39 +0530

    p7

---
+++ src/components/Downloadpdf.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+ 
+function Donwnloadpdf() {
+    return (
+        <div className='d-flex align-items-center justify-content-center'>
+            <a className='btn btn-success'>Download </a>
+
+        </div>
+
+    )
+   
+
+}
+
+export default Downloadpdf
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import HomeSlider from './HomeSlider'
 import WhyChooseUs from './WhyChooseUs'
+import Downloadpdf from './Downloadpdf'
+import { root } from 'postcss'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+ root.render(
+        <Downloadpdf
+        />
+);
 
 function Home({setActivePage}) {
     return (
