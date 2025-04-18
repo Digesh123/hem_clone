22076e51cc05239f7350769d6065b12d1dd640c5
Digesh123 <digeshghevade123@gmail.com>
2025-04-19 02:19:37 +0530

    P8

--- src/components/Downloadpdf.jsx
+++ src/components/Downloadpdf.jsx
@@ -1,12 +1,10 @@
 
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
- 
+
 function Donwnloadpdf() {
     return (
         <div className='d-flex align-items-center justify-content-center'>
             <a className='btn btn-success'>Download </a>
-
         </div>
 
     )
