5956fa27ea6e92e37ea34dcce2199412f51a639f
Digesh123 <digeshghevade123@gmail.com>
2025-04-19 02:29:13 +0530

    p12

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import HomeSlider from './HomeSlider'
 import WhyChooseUs from './WhyChooseUs'
 import Downloadpdf from './Downloadpdf'
-import { root1 } from 'postcss'
 
 const approot = ReactDOM.createRoot(document.getElementById('root1'));
  approot.render(
